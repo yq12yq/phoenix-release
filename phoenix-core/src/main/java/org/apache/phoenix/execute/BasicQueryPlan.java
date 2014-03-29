68113eaf9864cff24d8aa9bf7e5136b380f0e848
Jeffrey Zhong <jzhong@JZhongs-MacBook-Pro.local>
2014-03-29 11:23:32 -0700

    BUG-15407: Phoenix tests fail with AssertionError

--- phoenix-core/src/main/java/org/apache/phoenix/execute/BasicQueryPlan.java
+++ phoenix-core/src/main/java/org/apache/phoenix/execute/BasicQueryPlan.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.phoenix.compile.ExplainPlan;
 import org.apache.phoenix.compile.GroupByCompiler.GroupBy;
@@ -139,7 +140,9 @@ public abstract class BasicQueryPlan implements QueryPlan {
         // TODO: include time range in explain plan?
         PhoenixConnection connection = context.getConnection();
         Long scn = connection.getSCN();
-        ScanUtil.setTimeRange(scan, scn == null ? context.getCurrentTime() : scn);
+        // Add one to server time since max of time range is exclusive
+        // and we need to account of OSs with lower resolution clocks.
+        ScanUtil.setTimeRange(scan, scn == null ? context.getCurrentTime() + 1: scn);
         ScanUtil.setTenantId(scan, connection.getTenantId() == null ? null : connection.getTenantId().getBytes());
         ResultIterator iterator = newIterator();
         return dependencies.isEmpty() ? 
