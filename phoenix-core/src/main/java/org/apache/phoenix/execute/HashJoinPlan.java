a6c36a9c292aaf37de9d9e70b934e88993b3c0fb
Alicia Ying Shu <ashu99@gmail.com>
2015-06-04 12:34:03 -0700

    Revert "NPE BUG-34478 java.sql.SQLException: Encountered exception in sub plan [0] execution"
    This reverts commit 39907162e9ea2c26465b7a577fc6327489c53a6b.

--- phoenix-core/src/main/java/org/apache/phoenix/execute/HashJoinPlan.java
+++ phoenix-core/src/main/java/org/apache/phoenix/execute/HashJoinPlan.java
@@ -61,11 +61,11 @@ import org.apache.phoenix.parse.SelectStatement;
 import org.apache.phoenix.query.ConnectionQueryServices;
 import org.apache.phoenix.query.QueryServices;
 import org.apache.phoenix.query.QueryServicesOptions;
-import org.apache.phoenix.schema.PTable;
-import org.apache.phoenix.schema.tuple.Tuple;
 import org.apache.phoenix.schema.types.PArrayDataType;
 import org.apache.phoenix.schema.types.PBoolean;
 import org.apache.phoenix.schema.types.PDataType;
+import org.apache.phoenix.schema.PTable;
+import org.apache.phoenix.schema.tuple.Tuple;
 import org.apache.phoenix.util.SQLCloseable;
 import org.apache.phoenix.util.SQLCloseables;
 
@@ -280,22 +280,7 @@ public class HashJoinPlan extends DelegateQueryPlan {
                 rowCount++;
             }
             
-            Object result = null;
-            if (expectSingleRow) {
-                result = values.isEmpty() ? null : values.get(0);
-            } else {
-                if (rowCount == 0) {
-                    List<Expression> expressions = Lists.<Expression>newArrayListWithExpectedSize(columnCount);
-                    for (int i = 0; i < columnCount; i++) {
-                        ColumnProjector columnProjector = projector.getColumnProjector(i);
-                        PDataType type = columnProjector.getExpression().getDataType();
-                        expressions.add(LiteralExpression.newConstant(null, type));
-                    }
-                    Expression expression = new RowValueConstructorExpression(expressions, true);
-                    baseType = expression.getDataType();
-                }
-                result = PArrayDataType.instantiatePhoenixArray(baseType, values.toArray());
-            }
+            Object result = expectSingleRow ? (values.isEmpty() ? null : values.get(0)) : PArrayDataType.instantiatePhoenixArray(baseType, values.toArray());
             parent.getContext().setSubqueryResult(select, result);
             return null;
         }
