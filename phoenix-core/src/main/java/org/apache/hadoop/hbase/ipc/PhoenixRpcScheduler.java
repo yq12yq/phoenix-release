9a2891d88ea8b56067a7513ca90568bb3e1c9f66
tedyu <yuzhihong@gmail.com>
2016-11-02 17:16:10 -0700

    BUG-68340 fix build failure - addendum adds import
    Change-Id: I4885c44b6f747240671229fb19666d4b9106455e

--- phoenix-core/src/main/java/org/apache/hadoop/hbase/ipc/PhoenixRpcScheduler.java
+++ phoenix-core/src/main/java/org/apache/hadoop/hbase/ipc/PhoenixRpcScheduler.java
@@ -20,6 +20,7 @@ package org.apache.hadoop.hbase.ipc;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.ipc.SimpleRpcScheduler;
 import org.apache.phoenix.query.QueryServices;
 import org.apache.phoenix.query.QueryServicesOptions;
 
