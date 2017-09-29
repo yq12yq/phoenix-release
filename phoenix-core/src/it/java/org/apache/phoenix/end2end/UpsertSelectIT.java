cf6cd5ba7cd1b229e55902a4d6b203c6a0ff1841
Samarth Jain <samarth@apache.org>
2017-09-28 22:25:32 -0700

    BUG-115188. PHOENIX-4243 Using rowtimestamp column with RVC causes an exception sometimes

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/UpsertSelectIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/UpsertSelectIT.java
@@ -1164,7 +1164,6 @@ public class UpsertSelectIT extends BaseClientManagedTimeIT {
             assertEquals("KV3", rs.getString("KV3"));
             assertEquals(new Date(upsertedTs), rs.getDate("PK2"));
             assertFalse(rs.next());
-
             // Query using the index on base table
             stmt = conn.prepareStatement("SELECT KV1 FROM  " + baseTable + " WHERE PK2 = ? AND KV3 = ?");
             stmt.setDate(1, new Date(upsertedTs));
