apache/phoenix

PHOENIX-4762 Performance regression with transactional immutable indexes

phoenix-core/src/main/java/org/apache/phoenix/execute/PhoenixTxIndexMutationGenerator.java
@@ -95,7 +95,7 @@ public class PhoenixTxIndexMutationGenerator {
         stored.addAll(m);
     }
 
-    public Collection<Pair<Mutation, byte[]>> getIndexUpdates(HTableInterface htable, Iterator<Mutation> mutationIterator) throws IOException, SQLException {
+    public Collection<Pair<Mutation, byte[]>> getIndexUpdates(HTableInterface htable, Iterator<? extends Mutation> mutationIterator) throws IOException, SQLException {
 
         if (!mutationIterator.hasNext()) {
             return Collections.emptyList();

phoenix-core/src/main/java/org/apache/phoenix/transaction/OmidTransactionContext.java
@@ -130,4 +130,10 @@ public class OmidTransactionContext implements PhoenixTransactionContext {
         // TODO Auto-generated method stub
         return null;
     }
+
+    @Override
+    public HTableInterface getTransactionalTableWriter(HTableInterface htable, PTable table) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }

phoenix-core/src/main/java/org/apache/phoenix/transaction/PhoenixTransactionContext.java
@@ -108,6 +108,11 @@ public interface PhoenixTransactionContext {
         public HTableInterface getTransactionalTable(HTableInterface htable, boolean isImmutable) {
             return null;
         }
+
+        @Override
+        public HTableInterface getTransactionalTableWriter(HTableInterface htable, PTable table) {
+            return null;
+        }
     };
     /**
      * 
@@ -225,4 +230,5 @@ public interface PhoenixTransactionContext {
     public PhoenixTransactionContext newTransactionContext(PhoenixTransactionContext contex, boolean subTask);
 
     public HTableInterface getTransactionalTable(HTableInterface htable, boolean isImmutable);
+    public HTableInterface getTransactionalTableWriter(HTableInterface htable, PTable table);
 }

phoenix-core/src/main/java/org/apache/phoenix/transaction/TephraTransactionContext.java
@@ -397,5 +397,21 @@ public class TephraTransactionContext implements PhoenixTransactionContext {
         this.addTransactionAware(transactionAwareHTable);
         return transactionAwareHTable;
     }
+
+    @Override
+    public HTableInterface getTransactionalTableWriter(HTableInterface htable, PTable table) {
+        boolean isIndex = table.getType() == PTableType.INDEX;
+        TransactionAwareHTable transactionAwareHTable = new TransactionAwareHTable(htable, table.isImmutableRows() || isIndex ? TxConstants.ConflictDetection.NONE : TxConstants.ConflictDetection.ROW);
+        // Don't add immutable indexes (those are the only ones that would participate
+        // during a commit), as we don't need conflict detection for these.
+        if (isIndex) {
+            transactionAwareHTable.startTx(getTransaction());
+        } else {
+            // Even for immutable, we need to do this so that an abort has the state
+            // necessary to generate the rows to delete.
+            this.addTransactionAware(transactionAwareHTable);
+        }
+        return transactionAwareHTable;
+    }
     
 }

yq12yq/phoenix-release

(cherry picked from commit eeea6c60f299de73d0ebfd83b779f19476aa1fa9)
Change-Id: Id21771a48ec6ff7ae0618868dea81a9d9f0aae04

phoenix-core/src/main/java/org/apache/phoenix/execute/PhoenixTxIndexMutationGenerator.java
@@ -94,7 +94,7 @@ public class PhoenixTxIndexMutationGenerator {
         stored.addAll(m);
     }
 
-    public Collection<Pair<Mutation, byte[]>> getIndexUpdates(Table htable, Iterator<Mutation> mutationIterator) throws IOException, SQLException {
+    public Collection<Pair<Mutation, byte[]>> getIndexUpdates(Table htable, Iterator<? extends Mutation> mutationIterator) throws IOException, SQLException {
 
         if (!mutationIterator.hasNext()) {
             return Collections.emptyList();

phoenix-core/src/main/java/org/apache/phoenix/transaction/OmidTransactionContext.java
@@ -130,4 +130,10 @@ public class OmidTransactionContext implements PhoenixTransactionContext {
         // TODO Auto-generated method stub
         return null;
     }
+
+    @Override
+    public Table getTransactionalTableWriter(Table htable, PTable table) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }

phoenix-core/src/main/java/org/apache/phoenix/transaction/PhoenixTransactionContext.java
@@ -109,6 +109,11 @@ public interface PhoenixTransactionContext {
         public Table getTransactionalTable(Table htable, boolean isImmutable) {
             return null;
         }
+
+        @Override
+        public Table getTransactionalTableWriter(Table htable, PTable table) {
+            return null;
+        }
     };
     /**
      * 
@@ -227,4 +232,5 @@ public interface PhoenixTransactionContext {
     public PhoenixTransactionContext newTransactionContext(PhoenixTransactionContext contex, boolean subTask);
 
     public Table getTransactionalTable(Table htable, boolean isImmutable);
+    public Table getTransactionalTableWriter(Table htable, PTable table);
 }

phoenix-core/src/main/java/org/apache/phoenix/transaction/TephraTransactionContext.java
@@ -398,5 +398,21 @@ public class TephraTransactionContext implements PhoenixTransactionContext {
         this.addTransactionAware(transactionAwareHTable);
         return transactionAwareHTable;
     }
+
+    @Override
+    public Table getTransactionalTableWriter(Table htable, PTable table) {
+        boolean isIndex = table.getType() == PTableType.INDEX;
+        TransactionAwareHTable transactionAwareHTable = new TransactionAwareHTable(htable, table.isImmutableRows() || isIndex ? TxConstants.ConflictDetection.NONE : TxConstants.ConflictDetection.ROW);
+        // Don't add immutable indexes (those are the only ones that would participate
+        // during a commit), as we don't need conflict detection for these.
+        if (isIndex) {
+            transactionAwareHTable.startTx(getTransaction());
+        } else {
+            // Even for immutable, we need to do this so that an abort has the state
+            // necessary to generate the rows to delete.
+            this.addTransactionAware(transactionAwareHTable);
+        }
+        return transactionAwareHTable;
+    }
     
 }